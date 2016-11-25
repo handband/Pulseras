5b37b8205afe385b3b489141ad379c0639a5de75
ccanalesb <cristian.canales@mail.udp.cl>
2016-11-25 13:10:31 -0300

    Arreglando el script de las laser y subiendo cambios al readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pulseras
-Recopilación y ejemplos para creación de pulseras, credenciales, pvc con data variable
+Recopilación y ejemplos para creación de pulseras, certificados, credenciales, pvc con data variable
+
+## pulseras-laser
+Contiene un script que permite crear pulseras de tipo laser con data variable con las siguientes variables importantes
+ * total : Define la cantidad de pulseras que se generarán
+ * start_from : Define el número desde el cual parte el código de barras y texto bajo el código
+ * counter : Controla la cantidad de pulseras por página que se imprimirán
+ * sec : Controla que el número de pulseras generadas no supere al total (creo que no es necesaria)
--- pulseras-laser/script_laser.rb
+++ pulseras-laser/script_laser.rb
@@ -4,7 +4,6 @@ require 'barby/barcode/code_128'
 require 'barby/outputter/png_outputter'
 require 'stringio'
 
-
 filename = "molde laser.pdf"
 
 #Funcion generadora de codigos de barra 128
@@ -24,57 +23,51 @@ Prawn::Document.generate("out.pdf", :template => filename) do |pdf|
 
 	pdf.font "BebasNeue.ttf"
 	total = 1
-	start_from = 4200 #numero del codigo de barras
-	start_from2 = 4200 #numero del texto bajo el codigo de barras
+	start_from = start_from2 = 4200 #numero del codigo de barras
 	counter = 0
 	sec = 0
 	
-
 	while total>sec do
-	
 		
 		if counter > 10 then
 			pdf.start_new_page(:template => filename)
 			counter = 0
-		
-		"0%011d" % (start_from)
-	    
-		insert_code pdf, "0%05d" % (sec+start_from+0), -2, 87
-		insert_code pdf, "0%05d" % (sec+start_from+1), 70, 87
-		insert_code pdf, "0%05d" % (sec+start_from+2), 143, 87
-		insert_code pdf, "0%05d" % (sec+start_from+3), 214, 87
-		insert_code pdf, "0%05d" % (sec+start_from+4), 285, 87
-		insert_code pdf, "0%05d" % (sec+start_from+5), 280+75+2, 87
-		insert_code pdf, "0%05d" % (sec+start_from+6), (280+75*2)-2, 87
-		insert_code pdf, "0%05d" % (sec+start_from+7), (280+75*3)-5, 87
-		insert_code pdf, "0%05d" % (sec+start_from+8), (280+75*4)-8, 87
-		insert_code pdf, "0%05d" % (sec+start_from+9), (280+75*5)-11, 87
-
-		sec += 10
-
-		dx = 0
-		y=85+22
-		box_width = 125
-		xi = 25
 			
-		pdf.text_box "0%05d" % (start_from2), :at => [xi+4,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+1), :at => [xi+74+3,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+2), :at => [xi+(74*2)+1,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+3), :at => [xi+(74*3)-2,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+4), :at => [xi+(74*4)-4,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+5), :at => [xi+(74*5)-7,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+6), :at => [xi+(74*6)-10,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+7), :at => [xi+(74*7)-12,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+8), :at => [xi+(74*8)-14,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
-		pdf.text_box "0%05d" % (start_from2+9), :at => [xi+(74*9)-16,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			insert_code pdf, "0%05d" % (sec+start_from+0), -2, 87
+			insert_code pdf, "0%05d" % (sec+start_from+1), 70, 87
+			insert_code pdf, "0%05d" % (sec+start_from+2), 143, 87
+			insert_code pdf, "0%05d" % (sec+start_from+3), 214, 87
+			insert_code pdf, "0%05d" % (sec+start_from+4), 285, 87
+			insert_code pdf, "0%05d" % (sec+start_from+5), 280+75+2, 87
+			insert_code pdf, "0%05d" % (sec+start_from+6), (280+75*2)-2, 87
+			insert_code pdf, "0%05d" % (sec+start_from+7), (280+75*3)-5, 87
+			insert_code pdf, "0%05d" % (sec+start_from+8), (280+75*4)-8, 87
+			insert_code pdf, "0%05d" % (sec+start_from+9), (280+75*5)-11, 87
 
-		# end
+			sec += 10
 
-		start_from2 += 10
-		
-		print start_from2
-	end
+			y = 107
+			box_width = 125
+			x = 25
+				
+			pdf.text_box "0%05d" % (start_from2), :at => [x+4,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+1), :at => [x+74+3,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+2), :at => [x+(74*2)+1,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+3), :at => [x+(74*3)-2,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+4), :at => [x+(74*4)-4,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+5), :at => [x+(74*5)-7,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+6), :at => [x+(74*6)-10,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+7), :at => [x+(74*7)-12,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+8), :at => [x+(74*8)-14,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+			pdf.text_box "0%05d" % (start_from2+9), :at => [x+(74*9)-16,y], :size => 14, :rotate => 90, :width => box_width, :height=> 12, :overflow => :shrink_to_fit, :disable_wrap_by_char => true
+
+			# end
+
+			start_from2 += 10
+			
+			puts start_from2
+		end
 	counter += 1	
 	end
-	print "terminé"
+	puts "terminé"
 end
